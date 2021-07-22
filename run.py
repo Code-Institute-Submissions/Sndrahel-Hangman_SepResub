01e6bcdf818e8b95d19e77b681247fd28642da55
Sndrahel <sandra.helander@hotmail.com>
2021-07-22 09:09:28 +0000

    test(game-core): testing different name variables

--- run.py
+++ run.py
@@ -34,7 +34,7 @@ def play_game(secret_team):
     print("\n")
 
     while not team_is_guessed and game_life > 0:
-        guess = input("Guess a character: ").upper()
+        guess = input("Guess a character: ").upper()  # check code /n needed to deploy in heroku
         if len(guess) == 1 and guess.isalnum():
             if guess in guessed_character:
                 print("\nYou already guessed that character", guess)
